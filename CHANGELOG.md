cdec8b5c5d1ac2bed4937969e1552a3dd8763a34
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-12-01 19:49:06 +0100

    support no-std

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,5 +7,6 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased]
 
+* Support `no_std` target (require to disable default features)
 
 [Unreleased]: https://github.com/jcornaz/collision2d/compare/...HEAD
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 //! A simple 2d collision detection library supporting simple primitive shapes only,
 //! and that is suitable for development on `no_std` targets.
+//!
+//! # Features
+//!
+//! `std`: *(enabled by default)* enable use of the standard library. Must be disabled for `no_std` crates.
